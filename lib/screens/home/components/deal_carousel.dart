3fc04496467484694d93bc9210f058469dffa49d
andrewackerman <andrew.ackie@gmail.com>
2019-01-02 18:42:37 -0700

    finished mocking main screen

--- lib/screens/home/components/deal_carousel.dart
+++ lib/screens/home/components/deal_carousel.dart
@@ -1,5 +1,9 @@
+import 'dart:math' as Math;
+
 import 'package:flutter/material.dart';
 import 'package:hifidelity/components/gradient_raised_button.dart';
+import 'package:hifidelity/model/timespan.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class DealCarousel extends StatefulWidget {
   @override
@@ -7,6 +11,49 @@ class DealCarousel extends StatefulWidget {
 }
 
 class _DealCarouselState extends State<DealCarousel> {
+  final data = [
+    DealData(
+      icon: 'assets/images/feather_icon.png',
+      name: 'Feather Coin',
+      normalCost: 1.0,
+      newCostFactor: 0.2,
+      tokenCount: 140,
+      timeRemaining: TimeSpan.fromTime(days: 2, hours: 16, minutes: 1),
+    ),
+    DealData(
+      icon: 'assets/images/aureus_icon.png',
+      name: 'Aureus',
+      normalCost: 1.0,
+      newCostFactor: 0.35,
+      tokenCount: 75,
+      timeRemaining: TimeSpan.fromTime(days: 7, hours: 0, minutes: 55),
+    ),
+    DealData(
+      icon: 'assets/images/monero_icon.png',
+      name: 'Monero',
+      normalCost: 1.0,
+      newCostFactor: 0.2,
+      tokenCount: 250,
+      timeRemaining: TimeSpan.fromTime(days: 39, hours: 4, minutes: 21),
+    ),
+  ];
+
+  var page = 0;
+
+  void leftCarouselButtonPressed() {
+    print('left');
+    setState(() {
+      page--;
+    });
+  }
+
+  void rightCarouselButtonPressed() {
+    print('right');
+    setState(() {
+      page++;
+    });
+  }
+
   Widget timeRemainingWidget(BuildContext cxt) {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 12),
@@ -28,7 +75,7 @@ class _DealCarouselState extends State<DealCarousel> {
                 ),
                 SizedBox(width: 12),
                 Text(
-                  '02d',
+                  '${data[page].timeRemaining.days.toString().padLeft(2, '0')}d',
                   style: TextStyle(
                     fontSize: 14,
                     color: Color.fromARGB(255, 56, 152, 185),
@@ -45,7 +92,7 @@ class _DealCarouselState extends State<DealCarousel> {
                 ),
                 SizedBox(width: 8),
                 Text(
-                  '16h',
+                  '${data[page].timeRemaining.hours.toString().padLeft(2, '0')}h',
                   style: TextStyle(
                     fontSize: 14,
                     color: Color.fromARGB(255, 56, 152, 185),
@@ -62,7 +109,7 @@ class _DealCarouselState extends State<DealCarousel> {
                 ),
                 SizedBox(width: 8),
                 Text(
-                  '00m',
+                  '${data[page].timeRemaining.minutes.toString().padLeft(2, '0')}m',
                   style: TextStyle(
                     fontSize: 14,
                     color: Color.fromARGB(255, 56, 152, 185),
@@ -77,8 +124,146 @@ class _DealCarouselState extends State<DealCarousel> {
     );
   }
 
-  Widget carousel(BuildContext cxt) {
-    return Container(color: Colors.blue, height: 300);
+  Widget carouselWidget(BuildContext cxt) {
+    return Container(
+      height: 300,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: InkWell(
+              onTap: page > 0 ? leftCarouselButtonPressed : null,
+              child: Transform.rotate(
+                angle: Math.pi,
+                child: page > 0 ? Image.asset('assets/images/carousel_button.png') : Image.asset('assets/images/carousel_button_disabled.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 6,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 8,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                        child: Image.asset(data[page].icon),
+                      ),
+                      Text(
+                        data[page].name,
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Center(
+                    child: AutoSizeText(
+                      'at a',
+                      minFontSize: 1,
+                      maxFontSize: 999,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 8,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      LayoutBuilder(
+                        builder: (cxt, constraints) { 
+                          print(constraints.biggest.width);
+                          return Container(
+                            width: constraints.biggest.width,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  (100 - (data[page].newCostFactor * 100)).toInt().toString(),
+                                  style: TextStyle(
+                                    fontSize: 72 * (constraints.biggest.width / 130),
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.grey[800],
+                                  ),
+                                ),
+                                Text(
+                                  '%',
+                                  style: TextStyle(
+                                    fontSize: 18 * (constraints.biggest.width / 130),
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.grey[800],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                      ),
+                      AutoSizeText(
+                        'DISCOUNT',
+                        maxLines: 1,
+                        minFontSize: 1,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(5),
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: '\$${data[page].normalCost.toStringAsFixed(2)}',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  decoration: TextDecoration.lineThrough,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              TextSpan(
+                                text: ' ',
+                              ),
+                              TextSpan(
+                                text: '\$${(data[page].normalCost * data[page].newCostFactor).toStringAsFixed(2)}',
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 56, 152, 185),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ]
+                          ),
+                        ),
+                      ),
+                      AutoSizeText(
+                        'ON THE DOLLAR',
+                        maxLines: 1,
+                        minFontSize: 1,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: InkWell(
+              onTap: page < data.length - 1 ? rightCarouselButtonPressed : null,
+              child: page < data.length - 1 ? Image.asset('assets/images/carousel_button.png') : Image.asset('assets/images/carousel_button_disabled.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget getDealWidget(BuildContext cxt) {
@@ -98,7 +283,7 @@ class _DealCarouselState extends State<DealCarousel> {
                   text: 'Buy up to ',
                 ),
                 TextSpan(
-                  text: '140',
+                  text: data[page].tokenCount.toString(),
                   style: TextStyle(
                     color: Color.fromARGB(255, 56, 152, 185),
                     fontWeight: FontWeight.bold,
@@ -144,9 +329,27 @@ class _DealCarouselState extends State<DealCarousel> {
     return Column(
       children: [
         timeRemainingWidget(cxt),
-        carousel(cxt),
+        carouselWidget(cxt),
         getDealWidget(cxt),
       ]
     );
   }
+}
+
+class DealData {
+  final String icon;
+  final String name;
+  final double normalCost;
+  final double newCostFactor;
+  final int tokenCount;
+  final TimeSpan timeRemaining;
+
+  DealData({
+    this.icon,
+    this.name,
+    this.normalCost,
+    this.newCostFactor,
+    this.tokenCount,
+    this.timeRemaining,
+  });
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hifidelity/components/gradient_raised_button.dart';
-import 'package:hifidelity/components/iphonex_scaffold.dart';
 import 'package:hifidelity/screens/home/components/deal_carousel.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:hifidelity/components/gradient_progress_indicator.dart';
